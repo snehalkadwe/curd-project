33be1e0512483656e08cc96711cacf6f62a53d9f
snehal <snehalkadwe@gmail.com>
2022-07-27 23:45:49 +0530

    changed config

--- db_config.js
+++ db_config.js
@@ -1,15 +1,14 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var con = mysql.createConnection({
-  host: "Localhost",
+  host: "localhost",
   user: "root",
   password: "",
   database: "nodeDB",
 });
 
-
-// to create db 
-con.connect(function(err) {
+// to create db
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
   // con.query("CREATE DATABASE nodeDB", function (err, result) {
